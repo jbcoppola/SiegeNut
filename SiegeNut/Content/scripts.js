778e5c0029465b1a1b61d356ec0426d5d7fd314a
Joseph Coppola <jbcoppola@gmail.com>
2017-10-04 12:26:17 -0700

    got basic lightbox working

--- SiegeNut/Content/scripts.js
+++ SiegeNut/Content/scripts.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-
+    //ratings scripts
     var currentRating = $('.ratings').children('input').val();
     var v;
     $('.ratings img').on('mouseover', function () {
@@ -9,13 +9,55 @@
         StarUpdate(this, currentRating); //reverts to default rating or selected rating
     });
 
-    /* updates current rating with what's clicked */
     $('.ratings img').on('click', function () {
-        $(this).parent().children().animate({ backgroundColor: 'white' },0).animate({ backgroundColor: 'transparent' },"fast"); //highlight on click
-        currentRating = $(this).attr('value'); // The star currently selected
+        $(this).parent().children().animate({ backgroundColor: 'white' }, 0).animate({ backgroundColor: 'transparent' }, "fast"); //highlight on click
+        currentRating = $(this).attr('value'); // store value of currently selected star
         $("#Rating").val(currentRating); //changes value of hidden field
     });
-});
+    //lightbox script
+    var src;
+    $('.img-responsive').on('click', function () {
+        //Get clicked link src
+        src = $(this).attr("src");
+
+        /* 	
+		If the lightbox window HTML already exists in document, 
+		change the img src to to match the href of whatever link was clicked
+		
+		If the lightbox window HTML doesn't exist, create it and insert it.
+		(This will only happen the first time around)
+		*/
+
+        if ($('#lightbox').length > 0) { // #lightbox exists
+
+            //place href as img src value
+            $('#content img').attr('src', src);
+            console.log(src);
+
+            //show lightbox window - you could use .show('fast') for a transition
+            $('#lightbox').show();
+        }
+
+        else { //#lightbox does not exist - create and insert (runs 1st time only)
+
+            //create HTML markup for lightbox window
+            var lightbox =
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<img src="' + src + '" />' +
+				'</div>' +
+			'</div>';
+
+            //insert lightbox HTML into page
+            $('body').append(lightbox);
+        }
+
+        $('#lightbox').on('click', function () { //must use live, as the lightbox element is inserted into the DOM
+            $('#lightbox').hide();
+        });
+
+    });
 
 function StarUpdate(f, rating) {
     $(f).parent().children().each(function (e) {
@@ -29,4 +71,6 @@ function StarUpdate(f, rating) {
             else { $(this).attr('src', '/Images/half empty star right.png') }
         }
     });
-};
+}
+
+});
