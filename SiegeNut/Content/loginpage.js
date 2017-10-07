8e190ae6df55b23591272a6e40ce21028b71e71e
Joseph Coppola <jbcoppola@gmail.com>
2017-10-07 13:11:58 -0700

    made login and register page show and hide forms

---
+++ SiegeNut/Content/loginpage.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#register').hide();
+    $('.btn').click(function () {
+        if ($(this).attr('value') == "Login") {
+            $('#register').hide();
+            $('#login').show();
+        }
+        else if ($(this).attr('value') == "Register") {
+            $('#register').show();
+            $('#login').hide();
+        }
+    });
+});
