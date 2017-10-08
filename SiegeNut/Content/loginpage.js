914619d8679710356c055c881ac88ba289ba7289
Joseph Coppola <jbcoppola@gmail.com>
2017-10-07 17:01:17 -0700

    styled login form fully

--- SiegeNut/Content/loginpage.js
+++ SiegeNut/Content/loginpage.js
@@ -1,13 +1,22 @@
 ﻿$(document).ready(function () {
-    $('#register').hide();
     $('.btn').click(function () {
         if ($(this).attr('value') == "Login") {
+            if (!$(this).hasClass("selected")) {
+                $(this).addClass("selected");
+                $(".btn[value='Register']").removeClass("selected");
+            }
             $('#register').hide();
             $('#login').show();
         }
         else if ($(this).attr('value') == "Register") {
+            if (!$(this).hasClass("selected")) {
+                $(this).addClass("selected");
+                $(".btn[value='Login']").removeClass("selected");
+            }
             $('#register').show();
             $('#login').hide();
         }
     });
+
+    $('.default').click(); //whichever button is default shows that form
 });
